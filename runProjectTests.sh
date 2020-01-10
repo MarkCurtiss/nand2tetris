680334d67920d62c2046d7abdc033f1fda54e2cb
Mark Curtiss <markcurtiss@gmail.com>
2020-01-09 20:30:14 -0800

    Finished AND

--- runProjectTests.sh
+++ runProjectTests.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+set -e
+
+if [ $# -eq 0 ]
+then
+    echo "Please provide the project directory you wish to test (e.g. 01)"
+    exit
+fi
+
 PROJECT_NO=$1
 
 for TEST_FILE in projects/$PROJECT_NO/*.tst ;
