3c54849ad5524c332bd70269ce032d18cc1d7c3b
Mark Curtiss <markcurtiss@gmail.com>
2020-01-30 20:57:44 -0800

    I still think this assembly is weird

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,21 +3,6 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
-// Runs an infinite loop that listens to the keyboard input.
-// When a key is pressed (any key), the program blackens the screen,
-// i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed.
-// When no key is pressed, the program clears the screen, i.e. writes
-// "white" in every pixel;
-// the screen should remain fully clear as long as no key is pressed.
-
-	//Screen The Hack computer includes a black-and-white screen organized as 256 rows of 512 pixels per row.
-	//The screen’s contents are represented by an 8K memory map that starts at RAM address 16384 (0x4000).
-	//Each row in the physical screen, starting at the screen’s top left corner, is represented in the RAM by 32 consecutive 16-bit words.
-	//Thus the pixel at row r from the top and column c from the left is mapped on the c%16 bit (counting from LSB to MSB) of the word located at RAM[16384 + r · 32 + c/16].
-	//To write or read a pixel of the physical screen, one reads or writes the corresponding bit in the RAM-resident memory map (1 = black, 0 = white).
-
-
 	@SCREEN		//currentpixel = screen
 	D=A
 	@CURRENTPIXEL
@@ -25,37 +10,45 @@
 
 	(LOOP)
 	@KBD    // if keypress then FILL else CLEAR
-	D=M	// poll keyboard
+	D=M 	// poll keyboard
 	@FILL
 	D;JGT	// keypress detected, fill
 	@CLEAR
 	0;JMP   // else, clear screen
 
 	(FILL)
-	@CURRENTPIXEL
-	D=M
+    @CURRENTPIXEL   //if currentpixel = screensize == 0
+    D=M
+    @KBD
+    D=D-A
 
+    @LOOP           //resume input loop
+    D;JEQ
 
-
-	@CURRENTPIXEL	//currentpixel++
-	A=A+1
+	@CURRENTPIXEL   //otherwise, set the current pixel
+    A=M
+    M=-1
+    D=A
+    @CURRENTPIXEL
+    M=D+1            //set currentpixel to point to the next pixel
 
 	@LOOP
 	0;JMP
 
-	(CLEAR)  // while (i = 0; i < 256*512; i++) {
-	@i
-	M=0
-	D=M
-	@131072  // i < 256*512
-	D=D-A
-	@LOOP
-	D;JEQ   // go to loop
-	@i
-	D=M
-	@SCREEN  // screen[i] = 0;
-	A=D+A
-	M=0
+	(CLEAR)
+    @CURRENTPIXEL   // if currentpixel <= screen, then exit
+    D=M
+    @SCREEN
+    D=D-A
+    @LOOP
+    D;JLT
 
-	@CLEAR
-	0;JMP   // continue loop
+	@CURRENTPIXEL
+    A=M
+    M=0
+    D=A
+    @CURRENTPIXEL
+    M=D-1
+
+    @LOOP
+    0;JMP
