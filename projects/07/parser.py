7fb061b1eaaad5c0b649729aec8ad478547a22dc
Mark Curtiss <markcurtiss@gmail.com>
2020-02-19 19:02:04 -0800

    This does not work

--- projects/07/parser.py
+++ projects/07/parser.py
@@ -1,6 +1,8 @@
 from enum import Enum
 
-# pg 144
+class ParseError(Exception):
+    pass
+
 
 class Command(Enum):
     C_ARITHMETIC = 0
@@ -18,7 +20,7 @@ class Parser:
     def __init__(input):
         this.input_file = open(input, 'r')
         this.next_command = None
-        this.curent_command = None
+        this.current_command = None
 
 
     def has_more_commands():
@@ -31,5 +33,30 @@ class Parser:
 
 
     def commandType():
-        # check current command and return a type
-        pass
+        if this.current_command in ['add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and', 'or', 'not']:
+            return Command.C_ARITHMETIC
+        elif this.current_command == 'push':
+            return Command.C_PUSH
+        elif this.current_command == 'pop':
+            return Command.C_POP
+        elif this.current_command == 'label':
+            return Command.C_LABEL
+        elif this.current_command == 'goto':
+            return Command.C_GOTO
+        elif this.current_command == 'if-goto':
+            return Command.C_IF
+        elif this.current_command == 'function':
+            return Command.C_FUNCTION
+        elif this.current_command == 'return':
+            return Command.C_RETURN
+        elif this.current_command == 'call':
+            return Command.C_CALL
+        else:
+            raise ParseError(f'Unrecognized command {this.current_command}')
+
+
+    def arg1():
+        if commandType() == Command.C_RETURN:
+            raise ParseError('Do not call arg1 on command type C_RETURN')
+        else:
+            operands = this.command.split()
