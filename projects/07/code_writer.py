60a412c85e8f6ad02135a65abd1492ecca7adf22
Mark Curtiss <markcurtiss@gmail.com>
2020-02-24 16:30:17 -0800

    minor formatting changes and starting on eq

--- projects/07/code_writer.py
+++ projects/07/code_writer.py
@@ -19,8 +19,9 @@ class CodeWriter:
 
 
     def writeArithmetic(self, operator):
+        assembly = []
         if operator == 'add':
-            assembly = [x+'\n' for x in [
+            assembly = [
                 '@SP',
                 'A=M-1',
                 'D=M',
@@ -30,9 +31,43 @@ class CodeWriter:
                 'D=A+1',
                 '@SP',
                 'M=D'
-                ]]
+            ]
+        elif operator == 'eq':
+            assembly = [
+                '@SP',     # 0: 256
+                'A=M-1',   # A: 255
+                'D=M',     # D: RAM[256]
+                'A=A-1',   # A: 254
+                'D=D-M',   # D: RAM[255] -- RAM[254]
+#//                'M=A',     # SP: 254
+                '@TRUE',   # A: true
+                'D;JEQ',   # JMP if eq
 
-            self.output_file.writelines(assembly)
+                '@FALSE',
+                '0;JMP',
+
+                '(TRUE)',
+                '@SP',
+                'A=M',
+                'M=1',
+                '@END',
+                '0;JMP',
+
+                '(FALSE)',
+                '@SP',
+                'A=M',
+                'M=0',
+                '@END',
+                '0;JMP',
+
+                '(END)'
+            ]
+
+
+
+        self.output_file.writelines([x + '\n' for x in assembly])
+
+
 
 
         self.output_file.flush()
@@ -41,14 +76,13 @@ class CodeWriter:
         operator, label, operand = command.split()
         if(operator == 'push'):
             assembly = [x+'\n' for x in [
-                f'@{operand}',
-                'D=A',
-                '@SP',
-                'A=M',
-                'M=D',
-                'D=A+1',
-                '@SP',
-                'M=D'
+                f'@{operand}', # put constant in A
+                'D=A',         # D = constant
+                '@SP',         # 0 in A
+                'A=M',         # A == 256
+                'M=D',         # RAM[256] = constant
+                '@SP',         # A == 256
+                'M=M+1'        # RAM[0] == 256
             ]]
 
             self.output_file.writelines(assembly)
