8a02f519cc86240206798e3c7921312ed359e531
Mark Curtiss <markcurtiss@gmail.com>
2020-03-03 20:47:16 -0800

    Add tests for arithmetic code_writer functions with an eye towards refactoring

--- projects/07/test-code-writer.py
+++ projects/07/test-code-writer.py
@@ -8,50 +8,219 @@ class CodeWriterTest(unittest.TestCase):
     def setUp(self):
         self.tmpdir = TemporaryDirectory()
         os.chdir(self.tmpdir.name)
-        print(f'now working in {self.tmpdir.name}')
+        self.assembly_filename = 'test_output'
+        self.code_writer = CodeWriter(self.assembly_filename)
+
 
     def __del__(self):
         self.tmpdir.cleanup()
 
 
-    def test_simple_add(self):
-        code_writer = CodeWriter('test_output')
-        code_writer.writePushPop('push constant 7')
-        code_writer.writePushPop('push constant 8')
-        code_writer.writeArithmetic('add')
+    def test_add(self):
+        self.code_writer.writeArithmetic('add')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D+M',
+            'M=D',
+            '@SP',
+            'M=M+1'
+        ])
 
-        with open('test_output', 'r') as f:
-            assembly = [x.rstrip() for x in f.readlines()]
 
-            self.assertEqual(assembly, [
-                # push constant 7
-                '@7',
-                'D=A',
-                '@SP',
-                'A=M',
-                'M=D',
-                'D=A+1',
-                '@SP',
-                'M=D',
+    def test_sub(self):
+        self.code_writer.writeArithmetic('sub')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M-D',
+            'M=D',
+            '@SP',
+            'M=M+1'
+        ])
 
-                #push constant 8
-                '@8',
-                'D=A',
-                '@SP',
-                'A=M',
-                'M=D',
-                'D=A+1',
-                '@SP',
-                'M=D',
 
-                # add
-                '@SP',
-                'A=M-1',
-                'D=M',
-                'A=A-1',
-                'D=D+M',
-                'M=D',
-                'D=A+1',
-                '@SP',
-                'M=D'
-            ])
+    def test_neq(self):
+        self.code_writer.writeArithmetic('neg')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'M=-M',
+            '@SP',
+            'M=M+1'
+        ])
+
+
+    def test_and(self):
+        self.code_writer.writeArithmetic('and')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'M=M&D',
+            '@SP',
+            'M=M+1'
+        ])
+
+
+    def test_or(self):
+        self.code_writer.writeArithmetic('or')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'M=M|D',
+            '@SP',
+            'M=M+1'
+        ])
+
+
+    def test_not(self):
+        self.code_writer.writeArithmetic('not')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'M=!M',
+            '@SP',
+            'M=M+1'
+        ])
+
+
+    def test_eq(self):
+        self.code_writer.writeArithmetic('eq')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D-M',
+            '@test_output.1',
+            'D;JEQ',
+
+            '@test_output.0',
+            '0;JMP',
+
+            '(test_output.1)',
+            '@SP',
+            'A=M',
+            'M=-1',
+            '@test_output.2',
+            '0;JMP',
+
+            '(test_output.0)',
+            '@SP',
+            'A=M',
+            'M=0',
+            '@test_output.2',
+            '0;JMP',
+
+            '(test_output.2)',
+            '@SP',
+            'M=M+1'
+        ])
+
+
+    def test_lt(self):
+        self.code_writer.writeArithmetic('lt')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M-D',
+            '@test_output.1',
+            'D;JLT',
+
+            '@test_output.0',
+            '0;JMP',
+
+            '(test_output.1)',
+            '@SP',
+            'A=M',
+            'M=-1',
+            '@test_output.2',
+            '0;JMP',
+
+            '(test_output.0)',
+            '@SP',
+            'A=M',
+            'M=0',
+            '@test_output.2',
+            '0;JMP',
+
+            '(test_output.2)',
+            '@SP',
+            'M=M+1'
+        ])
+
+
+    def test_gt(self):
+        self.code_writer.writeArithmetic('gt')
+        self.assertGeneratedAssemblyEqual([
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=M',
+            '@SP',
+            'M=M-1',
+            'A=M',
+            'D=D-M',
+            '@test_output.1',
+            'D;JLT',
+
+            '@test_output.0',
+            '0;JMP',
+
+            '(test_output.1)',
+            '@SP',
+            'A=M',
+            'M=-1',
+            '@test_output.2',
+            '0;JMP',
+
+            '(test_output.0)',
+            '@SP',
+            'A=M',
+            'M=0',
+            '@test_output.2',
+            '0;JMP',
+
+            '(test_output.2)',
+            '@SP',
+            'M=M+1'
+        ])
+
+
+    def assertGeneratedAssemblyEqual(self, assembly=[]):
+        self.assertAssemblyEqual(self.assembly_filename, assembly)
+
+
+    def assertAssemblyEqual(self, file_name, assembly=[]):
+        with open(file_name, 'r') as f:
+            self.assertEqual([x.rstrip() for x in f.readlines()], assembly)
